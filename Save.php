42f149f46fb3a0a835dcfc9484f5e955dd88cecf
ArditGjyrevci <ardit.gjyrevci@student.uni-pr.edu>
2023-05-14 16:14:51 +0200

    commit4

--- Save.php
+++ Save.php
@@ -8,23 +8,18 @@
 		//check if form has been submitted
 		if(isset($_POST['submit'])){
 
-			//connect to database
-			$db_host = "localhost";
-			$db_user = "your_username";
-			$db_pass = "your_password";
-			$db_name = "your_database_name";
-			$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-			if (!$conn) {
-    			die("Connection failed: " . mysqli_connect_error());
-			}
+			include 'database.php';
 
 			//sanitize input data
-			$name = mysqli_real_escape_string($conn, $_POST['name']);
-			$email = mysqli_real_escape_string($conn, $_POST['email']);
-			$message = mysqli_real_escape_string($conn, $_POST['message']);
+			$courseName = mysqli_real_escape_string($conn, $_POST['courseName']);
+			$description = mysqli_real_escape_string($conn, $_POST['description']);
+			$price = mysqli_real_escape_string($conn, $_POST['price']);
+			$startDate = mysqli_real_escape_string($conn, $_POST['startDate']);
+			$endDate = mysqli_real_escape_string($conn, $_POST['endDate']);
+			
 
 			//insert data into database
-			$sql = "INSERT INTO messages (name, email, message) VALUES ('$name', '$email', '$message')";
+			$sql = "INSERT INTO course (courseName, description , price, startDate, endDate) VALUES ('$courseName', '$description', '$price','$startDate', '$endDate')";
 			if(mysqli_query($conn, $sql)){
 				echo "<p>Message saved successfully!</p>";
 			} else {
@@ -38,13 +33,19 @@
 
 	<form method="post" action="">
 		<label>Name:</label>
-		<input type="text" name="name"><br>
+		<input type="text" name="courseName"><br>
 
-		<label>Email:</label>
-		<input type="email" name="email"><br>
+		<label>Description:</label>
+		<input type="textfield" name="description"><br>
 
-		<label>Message:</label>
-		<textarea name="message"></textarea><br>
+		<label>Price:</label>
+		<input type="number" name="price"/>
+
+		<label>startDate:</label>
+		<input  type="date" name="startDate"/>
+
+		<label>enddate:</label>
+		<input  type="date" name="endDate"/>
 
 			<input type="submit" name="submit" value="Save">
 	</form>
